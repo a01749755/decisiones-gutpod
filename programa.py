d3ce86a2576059eca1fd6c10ea59f0ab34a27a7d
a01749755 <a01749755@tec.mx>
2022-08-17 13:51:48 +0000

    hola

---
+++ programa.py
@@ -0,0 +1,13 @@
+#comparacion de tres monedas 
+moneda1=int(input('Valor de la moneda: '))
+moneda2=int(input('Valor de la moneda: '))
+if(moneda1<moneda2):
+    menorvalor=moneda1
+else:
+    menorvalor=moneda2
+moneda3=int(input('Valor de la moneda: '))
+if(moneda3<menorvalor):
+        menorvalor=moneda3    
+print(f'La moneda con menor valor es {menorvalor}')
+    
+
